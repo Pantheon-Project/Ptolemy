d06ac11670c6cf85fd0273f696c064a2e2843340
Luke Swanson <lwswanson2@wisc.edu>
2022-07-06 23:43:50 -0500

    Set up a class to manage sets of colors.

---
+++ Colorbook.py
@@ -0,0 +1,53 @@
+from enum import Enum
+from typing import List, Tuple, Union
+
+_ColorType = Union[Tuple[int, int, int], Tuple[int, int, int, int]]
+
+class Colorbook:
+    """Class to store a collection of colors, to be accessed with the "Next" function.
+    The Colorbook "mode" sets the method for iterating through the list.
+    """
+    class MODE(Enum):
+        """Enumerated type to set the mode of advancing through the Colorbook.
+        """
+        CONSTANT = 1
+        LINEAR = 2
+        BINARY_TREE  = 3
+        TRINARY_TREE = 4
+
+    def __init__(self, color_list:List[_ColorType], mode:MODE=MODE.LINEAR):
+        self._colors = color_list
+        self._mode   = mode
+        self._next   = 0
+
+    def __getitem__(self, index:int):
+        idx = index % len(self._colors)
+        return self._colors[idx]
+
+    def __setitem__(self, index:int, color:_ColorType):
+        if index < 0 or index > len(self._colors):
+            return
+        else:
+            self._colors[index] = color
+
+    def Append(self, color:_ColorType):
+        """Add a color to the Colorbook.
+
+        :param color: The color to add to the book.
+        :type color: _ColorType
+        """
+        self._colors.append(color)
+
+    @property
+    def Next(self):
+        """Get next color from the Colorbook, according to the book's mode.
+
+        :return: _description_
+        :rtype: _type_
+        """
+        ret_val = self._colors[self._next]
+        if self._mode == Colorbook.MODE.CONSTANT:
+            pass
+        elif self._mode == Colorbook.MODE.LINEAR:
+            self._next = (self._next + 1) % len(self._colors)
+        return ret_val
